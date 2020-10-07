1b67b2b75b00f9f8fbd26ab0f53d88fa31d3aa80
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-07 17:02:10 +0300

    blank sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,1 +1,22 @@
 import React from 'react';
+import './Sidebar.css';
+
+
+function Sidebar() {
+    return(
+        <div className="sidebar">
+            {/* Twitter icon */}
+
+            {/* Sidebar Option */}
+            {/* Sidebar Option */}
+            {/* Sidebar Option */}
+            {/* Sidebar Option */}
+            {/* Sidebar Option */}
+            {/* Sidebar Option */}
+
+            {/* Button -> Tweet */}
+        </div>
+    );
+}
+
+export default Sidebar;
