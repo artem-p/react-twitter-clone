eac0d6bf19b6e870be9e5627aaec76976a33850a
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-09 12:22:02 +0300

    move sidebar options to the proper place

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
-import SidebarOption from './SidebarOption';
-import HomeIcon from "@material-ui/icons/Home";
-import SearchIcon from "@material-ui/icons/Search";
-import NotificationsNoneIcon from "@material-ui/icons/Notifications";
-import MailOutlineIcon from "@material-ui/icons/MailOutline";
-import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
-import ListAltIcon from "@material-ui/icons/ListAlt";
-import PermIdentityIcon from "@material-ui/icons/PermIdentity";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 
 function App() {
@@ -19,14 +10,7 @@ function App() {
 
         <Sidebar />
 
-        <SidebarOption active Icon={HomeIcon} text="Home"/>
-        <SidebarOption Icon={SearchIcon} text="Explore"/>
-        <SidebarOption Icon={NotificationsNoneIcon}  text="Notifications"/>
-        <SidebarOption Icon={MailOutlineIcon} text="Messages" />
-        <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
-        <SidebarOption Icon={ListAltIcon} text="Lists" />
-        <SidebarOption Icon={PermIdentityIcon} text="Profile" />
-        <SidebarOption Icon={MoreHorizIcon} text="More" />
+        
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/Notifications";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 
 function Sidebar() {
@@ -9,12 +18,14 @@ function Sidebar() {
             {/* Twitter icon */}
             <TwitterIcon />
 
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
+            <SidebarOption active Icon={HomeIcon} text="Home"/>
+            <SidebarOption Icon={SearchIcon} text="Explore"/>
+            <SidebarOption Icon={NotificationsNoneIcon}  text="Notifications"/>
+            <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+            <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+            <SidebarOption Icon={ListAltIcon} text="Lists" />
+            <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+            <SidebarOption Icon={MoreHorizIcon} text="More" />
 
             {/* Button -> Tweet */}
         </div>
