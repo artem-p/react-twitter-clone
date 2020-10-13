00ef24e9e1acd08d3e6fa2735db1413797b6b0dc
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-13 12:51:40 +0300

    add avatar and button

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './TweetBox.css';
+import {Avatar, Button} from "@material-ui/core";
 
 
 function TweetBox() {
     return (
         <div className="tweetBox">
-            
+            <form>
+                <div className="tweetBox__input">
+                    <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+                </div>
+
+                <Button>Tweet</Button>
+            </form>
         </div>
     )
 }
