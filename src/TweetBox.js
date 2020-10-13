58e26ff40337aaa109b6f1417f4eda83dc85af7c
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-13 14:21:27 +0300

    style tweetbox input

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -9,6 +9,7 @@ function TweetBox() {
             <form>
                 <div className="tweetBox__input">
                     <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+                    <input placeholder="What's happening?" type="text"></input>
                 </div>
 
                 <Button>Tweet</Button>
