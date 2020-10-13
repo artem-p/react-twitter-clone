d3d5a1c92f21c39f35f120138d14b1f5cc2ede1e
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-13 14:33:06 +0300

    add image input field

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -11,6 +11,7 @@ function TweetBox() {
                     <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
                     <input placeholder="What's happening?" type="text"></input>
                 </div>
+                <input className="tweetBox__imageInput" placeholder="Optional: Enter image URL" type="text"></input>
 
                 <Button className="tweetBox__tweetButton">Tweet</Button>
             </form>
