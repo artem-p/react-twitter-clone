a369180a0dcf855843a77d8c65431abdff2c88c3
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-16 12:16:39 +0300

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+import { firebaseConfig } from './config';
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
