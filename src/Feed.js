1e8e893ef8ee89562132a030a903b61454ad2275
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-19 17:29:53 +0300

    populate posts from the database

--- src/Feed.js
+++ src/Feed.js
@@ -2,14 +2,16 @@ import React, {useState, useEffect} from 'react'
 import "./Feed.css";
 import TweetBox from './TweetBox';
 import Post from "./Post";
+import db from './firebase';
 
 
 function Feed() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        
-        
+        db.collection('posts').onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map(doc => doc.data()))
+        ))
     }, [])
 
     return (
