c52be6c59c59e42c4a02cf3bda598e117ea42236
Artyom Pugachev <artempugachev1@gmail.com>
2020-10-20 14:46:45 +0300

    add tweet image hook

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -5,6 +5,7 @@ import {Avatar, Button} from "@material-ui/core";
 
 function TweetBox() {
     const [tweetMessage, setTweetMessage] = useState("");
+    const [tweetImage, setTweetImage] = useState("");
 
     return (
         <div className="tweetBox">
@@ -13,14 +14,21 @@ function TweetBox() {
                     <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
                     
                     <input
-                        onChange={(e)=> setTweetMessage(e.target.value)} 
                         value={tweetMessage} 
+                        onChange={(e) => setTweetMessage(e.target.value)} 
                         placeholder="What's happening?" 
                         type="text">
                     </input>
 
                 </div>
-                <input className="tweetBox__imageInput" placeholder="Optional: Enter image URL" type="text"></input>
+                
+                <input
+                    value={tweetImage}
+                    onChange={(e) => setTweetImage(e.target.value)}
+                    className="tweetBox__imageInput" 
+                    placeholder="Optional: Enter image URL" 
+                    type="text">
+                </input>
 
                 <Button className="tweetBox__tweetButton">Tweet</Button>
             </form>
